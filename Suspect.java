2ed2a34029ba314326cc4a839421c68f8932b443
shbacon <77406398+shbacon@users.noreply.github.com>
2021-03-11 09:52:53 -0500

    Add files via upload

---
+++ Suspect.java
@@ -0,0 +1,32 @@
+package groupproj;
+
+public class Suspect {
+    protected String bodyType;
+    protected boolean isCriminal;
+    ArrayList<Relatives> relatives = new ArrayList<Relative>();
+
+    public Suspect() {
+
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public boolean getIsCriminal() {
+        return isCriminal;
+    }
+     
+    // get relatives String[]
+
+    public void setBodyType() {
+
+    }
+    
+    public void setIsCriminal() {
+
+    }
+
+    // set relatives
+
+}
