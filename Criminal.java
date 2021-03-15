95d57a5cd15c8b6ad48e9971f4926be072d7194e
nickbrady14 <77462666+nickbrady14@users.noreply.github.com>
2021-03-15 18:28:03 -0400

    Update Criminal.java
    updated getters and setters/need to fix functionality for setNumCrimes

--- Criminal.java
+++ Criminal.java
@@ -1,43 +1,49 @@
+import java.util.ArrayList;
 
-public class Criminal extends Person {
+public class Criminal extends Person{
     private boolean inJail;
     private String notes;
     private int numCrimes;
 
-    public Criminal(String witnessState, String Name, int Age,int Height,int Weight, String Gender,String Address,
-    String Phone, String DateOfBirth,boolean IsAdult,int id, boolean inJail, String notes, int numCrimes) {
-        super(Name, Age, Height, Weight,  Gender,Address,
+    public Criminal(String Name, int Age,int Height,int Weight, String Gender,String Address,
+    String Phone, String DateOfBirth,boolean IsAdult,int id, boolean inJail, String notes, int numCrimes){
+        super(Name, Age, Height, Weight,  Gender, Address,
         Phone,  DateOfBirth, IsAdult, id);
         this.inJail = inJail;
         this.notes = notes;
         this.numCrimes = numCrimes;
     }
 
-    public void setJail() {
-        /*
-         * if(this.Criminal.inJail == true) { inJail = true; } else { inJail = false; }
-         */
-    }
-
-    public void setNumCrimes() {
-        /*
-         * for(int i=0;i<cases.length;i++) { if(criminal.getCrimes[i] != null) {
-         * numCrimes++; } }
-         */
-    }
-
-    // set crimeTypes
-    // get crimeTypes String[]
-    public boolean getJail() {
+    public boolean getJail(){
         return inJail;
     }
 
-    public int getNumCrimes() {
-        return numCrimes;
+    public void setJail(boolean inJail){
+        if(this.inJail == true){ 
+            inJail = true; 
+        } 
+        else{ 
+            inJail = false; 
+        }
     }
 
-    public String getNotes() {
+    public String getNotes(){
         return notes;
     }
 
+    public void setNotes(String notes){
+        this.notes = notes;
+    }
+
+    public int getNumCrimes(){
+        return numCrimes;
+    }
+
+    public void setNumCrimes(ArrayList<Case> cases){
+        for(int i=0;i<cases.size();i++){ 
+            if(cases.get(i) != null && this.name.equals(cases.get(i).getCriminals().name)){
+                numCrimes++; 
+            }
+        }        
+    }
 }
