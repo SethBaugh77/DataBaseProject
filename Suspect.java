c3d49f74d37a677e285913eef16be196ee1b70b8
SethBaugh77 <eif8e8di@gmail.com>
2021-03-14 20:19:08 -0400

    Witness and Suspect methods

--- Suspect.java
+++ Suspect.java
@@ -1,43 +1,43 @@
 
-
 import java.util.ArrayList;
 
-public class Suspect extends PersonOfInterest{
+public class Suspect extends PersonOfInterest {
     protected String bodyType;
     protected boolean isCriminal;
     ArrayList<Suspect> relatives = new ArrayList<Suspect>();
 
-    public Suspect(String name, int age, int height, int weight, 
-    String gender, String address, String phone, String dateOfBirth, 
-    boolean isAdult, int id, String occupation, String crimeConnectionString, 
-    String lastLocation, String bodyType, boolean isCriminal, ArrayList<Suspect> relatives) {
-        super( name,  age,  height,  weight, 
-        gender,  address,  phone,  dateOfBirth, 
-         isAdult,  id,  occupation,  crimeConnectionString, 
-         lastLocation);
-         this.bodyType = bodyType;
-         this.isCriminal = isCriminal;
-         this.relatives = relatives;
+    public Suspect(String name, int age, int height, int weight, String gender, String address, String phone,
+            String dateOfBirth, boolean isAdult, int id, String occupation, String crimeConnectionString,
+            String lastLocation, String bodyType, boolean isCriminal, ArrayList<Suspect> relatives) {
+        super(name, age, height, weight, gender, address, phone, dateOfBirth, isAdult, id, occupation,
+                crimeConnectionString, lastLocation);
+        this.bodyType = bodyType;
+        this.isCriminal = isCriminal;
+        this.relatives = relatives;
     }
 
     public String getBodyType() {
-        return bodyType;
+        return this.bodyType;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
     }
 
     public boolean getIsCriminal() {
-        return isCriminal;
-    }
-     
-    // get relatives String[]
-
-    public void setBodyType() {
-
-    }
-    
-    public void setIsCriminal() {
-
+        return this.isCriminal;
     }
 
-    // set relatives
+    public void setIsCriminal(boolean isCriminal) {
+        this.isCriminal = isCriminal;
+    }
+
+    public ArrayList<Suspect> getRelatives() {
+        return this.relatives;
+    }
+
+    public void setRelatives(ArrayList<Suspect> relatives) {
+        this.relatives = relatives;
+    }
 
 }
