8f9265be371ece8e099d10431229a2ad1c266922
nickbrady14 <77462666+nickbrady14@users.noreply.github.com>
2021-03-23 13:36:18 -0400

    Update PersonOfInterest.java

--- PersonOfInterest.java
+++ PersonOfInterest.java
@@ -2,14 +2,14 @@
 public class PersonOfInterest extends Person {
     protected String occupation;
     protected String lastLocation;
-   // protected String poiNotes;
+    protected String poiNotes;
 
     public PersonOfInterest(String fname, String lname, long age, long height, long weight, String gender, String address,
             String phone, String dateOfBirth, boolean isAdult, String occupation, String lastLocation, long ID) {
         super(fname, lname, age, height, weight, gender, address, phone, dateOfBirth, isAdult);
         this.occupation = occupation;
         this.lastLocation = lastLocation;
-       // this.poiNotes = poiNotes;
+        this.poiNotes = poiNotes;
     }
 
     public String getOccupation() {
@@ -20,13 +20,13 @@ public class PersonOfInterest extends Person {
         this.occupation = occupation;
     }
 
-    // public String getpoiNotes() {
-    //     return poiNotes;
-    // }
+    public String getpoiNotes() {
+        return poiNotes;
+    }
 
-    // public void setpoiNotes(String poiNotes) {
-    //     this.poiNotes = poiNotes;
-    // }
+    public void setpoiNotes(String poiNotes) {
+        this.poiNotes = poiNotes;
+    }
 
     public String getLastLocation() {
         return lastLocation;
