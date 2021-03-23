65779832ec7e74653a71950010ecf58a2979eedc
shbacon <77406398+shbacon@users.noreply.github.com>
2021-03-23 11:03:51 -0400

    Add files via upload

---
+++ adminUser.java
@@ -0,0 +1,27 @@
+package groupproj;
+
+import java.util.ArrayList;
+
+public class AdminUser extends User {
+
+    AdminUser(int id, boolean admin, String fname, String lname, String username, String email, String password,
+            String phone, ArrayList<Criminal> Criminals, ArrayList<Case> _cases) {
+        super(id, admin, fname, lname, username, email, password, phone, Criminals, _cases);
+    }
+
+    public void removeCriminal(Criminal _crim) {
+        criminals.remove(_crim);
+
+    }
+
+    public void removeCase(Case _case) {
+        cases.remove(_case);
+
+    }
+
+    public void removeUser(User _user) {
+        users.remove(_user);
+
+    }
+
+}
