5a5515a384f51e4f4fccb339e34a5d94f6f9eaf6
shbacon <77406398+shbacon@users.noreply.github.com>
2021-03-28 09:33:19 -0400

    Update DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -1,5 +1,9 @@
 import java.util.*;
 
+ /**
+  * DataBase
+  * @return this dbase.
+  */
 public class DataBase {
 	public static DataBase getInstance() {
 		if (dbase == null)
@@ -31,76 +35,147 @@ public class DataBase {
 
 	}
 
+	 /**
+   	* Gets the evidence.
+   	* @return this evidence.
+   	*/
 	public ArrayList<Evidence> getEvidence() {
 		return this.evidence;
 	}
 
+	 /**
+   	* Changes the evidence.
+   	* @param arraylist evidence.  
+   	*/
 	public void setEvidence(ArrayList<Evidence> evidence) {
 		this.evidence = evidence;
 	}
 
+	 /**
+   	* Gets the criminal.
+   	* @return this criminal.
+   	*/
 	public ArrayList<Criminal> getCriminals() {
 
 		return this.criminals;
-
 	}
 
+	 /**
+   	* Changes the criminal.
+   	* @param arraylist criminal.  
+   	*/
 	public void setCriminals(ArrayList<Criminal> criminals) {
 		this.criminals = criminals;
 	}
 
+	 /**
+   	* Gets the case.
+   	* @return this case.
+   	*/
 	public ArrayList<Case> getCases() {
 		return this.cases;
 	}
 
+	 /**
+   	* Changes the case.
+   	* @param arraylist case.  
+   	*/
 	public void setCases(ArrayList<Case> cases) {
 		this.cases = cases;
 	}
 
+	 /**
+   	* Gets the user.
+   	* @return this user.
+   	*/
 	public ArrayList<User> getUsers() {
 		return this.users;
 	}
 
+	 /**
+   	* Changes the users.
+   	* @param arraylist users.  
+   	*/
 	public void setUsers(ArrayList<User> users) {
 		this.users = users;
 	}
 
+	 /**
+   	* Gets the victim.
+   	* @return this victim.
+   	*/
 	public ArrayList<Victim> getVictims() {
 		return this.victims;
 	}
 
+	 /**
+   	* Changes the victim.
+   	* @param arraylist victim.  
+   	*/
 	public void setVictims(ArrayList<Victim> victims) {
 		this.victims = victims;
 	}
 
+	 /**
+   	* Gets the witness.
+   	* @return this witness.
+   	*/
 	public ArrayList<Witness> getWitnesses() {
 		return this.witnesses;
 	}
 
+	 /**
+   	* Changes the witness.
+   	* @param arraylist witness.  
+   	*/
 	public void setWitnesses(ArrayList<Witness> witnesses) {
 		this.witnesses = witnesses;
 	}
 
+	 /**
+   	* Gets the officers.
+   	* @return this officer.
+   	*/
 	public ArrayList<Officer> getOfficers() {
 		return this.officers;
 	}
 
+	 /**
+   	* Changes the officers.
+   	* @param arraylist officer.  
+   	*/
 	public void setOfficers(ArrayList<Officer> officers) {
 		this.officers = officers;
 	}
 
+	 /**
+   	* Gets the POI.
+   	* @return this POI.
+   	*/
 	public ArrayList<PersonOfInterest> getPOI() {
 		return this.POI;
 	}
 
+	 /**
+   	* Changes the POI.
+   	* @param arraylist POI.  
+   	*/
 	public void setPOI(ArrayList<PersonOfInterest> POI) {
 		this.POI = POI;
 	}
 
+	 /**
+   	* Gets the suspect.
+   	* @return this suspect.
+   	*/
 	public ArrayList<Suspect> getSuspects() {
 		return this.suspects;
 	}
 
+	 /**
+   	* Changes the suspect.
+   	* @param arraylist suspect.  
+   	*/
 	public void setSuspects(ArrayList<Suspect> suspects) {
 		this.suspects = suspects;
 	}
